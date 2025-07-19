eda2d45be67ba88bd25d9f5624a91dd7accd3e22
maktrung <maktrung@gmail.com>
2025-07-19 18:44:17 +0700

    Update character logic

---
+++ Assets/Scripts/Character/CharacterStats.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CharacterStats : ScriptableObject
+{
+    public float MaxHealth = 100f;
+
+    public float AttackPower = 20f;
+
+    public float AttackCooldown = 1.5f;
+
+    public float MoveSpeed = 3f;
+
+    public bool HasRangeAttack = false;
+
+    public float RangeAttackPower = 15f;
+}
---
+++ Assets/Scripts/Game/GameEnum.cs
@@ -0,0 +1,8 @@
+﻿public enum FightMode : byte
+{
+    OneVsOne = 0,
+
+    OneVsMany = 1,
+
+    ManyVsMany = 2
+}
